55ea49d11319e0ea5fd41fb4ca31d43d1f90524c
James Waller <james.waller1984@gmail.com>
2020-04-19 12:37:25 +1000

    validation added to amount input.

--- src/components/Form.js
+++ src/components/Form.js
@@ -145,7 +145,7 @@ const Form = () => {
                 <Left>
                     <label>Amount: <sup>*</sup></label>
                     <input 
-                        type="amount" 
+                        type="number" 
                         name="amount" 
                         onChange={e => {setAmount(e.target.value)}} 
                         required
@@ -225,6 +225,14 @@ const Wrapper = styled.div`
             @media screen and (min-width: 321px) {
                 margin: 1rem;
             }
+        }
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+        input[type=number] {
+            -moz-appearance: textfield;
         }
         select {
             background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
